b40f9aefd2c803f5ceddba4b9cc0b60963cf4628
Jordon Leach <jordonleach@gmail.com>
2022-10-17 16:56:35 -0400

    Add steps for auto-installing cert-manager and kubewarden repository

---
+++ pkg/kubewarden/components/overview/InstallWizard.vue
@@ -0,0 +1,214 @@
+<script>
+export default {
+  name: 'InstallMenu',
+
+  props: {
+    getStartedLink: {
+      type:    Object,
+      default: null,
+    },
+
+    initStepIndex: {
+      type:    Number,
+      default: 0
+    },
+
+    steps: {
+      type:    Array,
+      default: null,
+    },
+  },
+
+  data() {
+    return { activeStep: null };
+  },
+
+  created() {
+    this.activeStep = this.steps[this.initStepIndex];
+  },
+
+  computed: {
+    activeStepIndex() {
+      return this.steps.findIndex(s => s.name === this.activeStep.name);
+    }
+  },
+
+  methods: {
+    goToStep(number, fromNav) {
+      if (number < 1) {
+        return;
+      }
+
+      if (number === 1 && fromNav) {
+        return;
+      }
+
+      const selected = this.steps[number - 1];
+
+      if (!selected || (!this.isAvailable(selected) && number !== 1)) {
+        return;
+      }
+
+      this.activeStep = selected;
+
+      this.$emit('next', { step: selected });
+    },
+
+    isAvailable(step) {
+      if (!step) {
+        return false;
+      }
+
+      const idx = this.steps.findIndex(s => s.name === step.name);
+
+      if (idx === 0) {
+        return false;
+      }
+
+      for (let i = 0; i < idx; i++) {
+        if (this.steps[i].ready === false) {
+          return false;
+        }
+      }
+
+      return true;
+    },
+
+    next() {
+      this.goToStep(this.activeStepIndex + 2);
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <div class="step-sequence mt-20">
+      <ul class="steps" tabindex="0">
+        <template v-for="(step, idx) in steps">
+          <li
+            :id="step.name"
+            :key="step.name + 'li'"
+            :class="{
+              step: true,
+              active: step.name === activeStep.name,
+              disabled: !isAvailable(step),
+            }"
+            role="presentation"
+          >
+            <span
+              :aria-controls="'step' + idx + 1"
+              :aria-selected="step.name === activeStep.name"
+              role="tab"
+              class="controls"
+              @click.prevent="goToStep(idx + 1, true)"
+            >
+              <span
+                class="icon icon-lg"
+                :class="{
+                  'icon-dot': step.name === activeStep.name,
+                  'icon-dot-open': step.name !== activeStep.name,
+                }"
+              />
+              <span>
+                {{ step.label }}
+              </span>
+            </span>
+          </li>
+          <div
+            v-if="idx !== steps.length - 1"
+            :key="step.name"
+            class="divider"
+          />
+        </template>
+      </ul>
+    </div>
+
+    <hr class="m-20" />
+
+    <slot class="step-container" name="stepContainer" :activeStep="activeStep">
+      <template v-for="step in steps">
+        <div
+          v-if="step.name === activeStep.name || step.hidden"
+          :key="step.name"
+          class="step-container__step"
+          :class="{ hide: step.name !== activeStep.name && step.hidden }"
+        >
+          <slot :step="step" :name="step.name" />
+        </div>
+      </template>
+    </slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.step-sequence {
+  flex: 1;
+  min-height: 60px;
+  display: flex;
+  width: 100%;
+
+  .steps {
+    flex: 1;
+    margin: 0 30px;
+    display: flex;
+    justify-content: space-between;
+    list-style-type: none;
+    padding: 0;
+
+    &:focus {
+      outline: none;
+      box-shadow: none;
+    }
+
+    & li.step {
+      display: flex;
+      flex-direction: row;
+      flex-grow: 1;
+      align-items: center;
+
+      & > span > span:last-of-type {
+        padding-bottom: 0;
+      }
+
+      &:last-of-type {
+        flex-grow: 0;
+      }
+
+      & .controls {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 40px;
+        overflow: visible;
+        padding-top: 15px;
+      }
+
+      &.active .controls {
+        color: var(--primary);
+      }
+
+      &:not(.disabled) {
+        & .controls:hover > * {
+          color: var(--primary) !important;
+          cursor: pointer;
+        }
+      }
+
+      &:not(.active) {
+        & .controls > * {
+          color: var(--input-disabled-text);
+          text-decoration: none;
+        }
+      }
+    }
+
+    & .divider {
+      flex-basis: 100%;
+      border-top: 1px solid var(--border);
+      position: relative;
+      top: 28px;
+    }
+  }
+}
+</style>
--- pkg/kubewarden/pages/c/_cluster/kubewarden/index.vue
+++ pkg/kubewarden/pages/c/_cluster/kubewarden/index.vue
@@ -1,53 +1,157 @@
 <script>
 import { mapGetters } from 'vuex';
-import { CATALOG } from '@shell/config/types';
+
+import { CATALOG, MANAGEMENT, SERVICE } from '@shell/config/types';
 import { REPO_TYPE, REPO, CHART, VERSION } from '@shell/config/query-params';
 
-import { Banner } from '@components/Banner';
-import CopyCode from '@shell/components/CopyCode';
+import AsyncButton from '@shell/components/AsyncButton';
 import Loading from '@shell/components/Loading';
+
+import InstallWizard from '~/pkg/kubewarden/components/overview/InstallWizard';
+
+const KUBEWARDEN_REPO = 'https://charts.kubewarden.io';
 
 export default {
   name: 'Dashboard',
 
   components: {
-    Banner, CopyCode, Loading
+    AsyncButton,
+    InstallWizard,
+    Loading,
   },
 
   async fetch() {
+    const whitelist = await this.$store.dispatch('management/find', { type: MANAGEMENT.SETTING, id: 'whitelist-domain' });
+
+    if ( !whitelist.value.includes('github.com') ) {
+      whitelist.default += ',github.com';
+      whitelist.value += ',github.com';
+      await whitelist.save();
+    }
+
+    const certManager = await this.$store.dispatch('cluster/find', { type: SERVICE, id: 'cert-manager/cert-manager' });
+
+    if ( certManager ) {
+      this.installSteps[0].ready = true;
+      this.initStepIndex = 1;
+    }
+
+    this.repoSchema = this.$store.getters['management/schemaFor'](CATALOG.CLUSTER_REPO);
+
+    if ( this.repoSchema ) {
+      const allRepos = await this.$store.dispatch('management/findAll', { type: CATALOG.CLUSTER_REPO });
+
+      if ( allRepos.find(r => r.spec.url === KUBEWARDEN_REPO) ) {
+        this.installSteps[1].ready = true;
+        this.initStepIndex = 2;
+      }
+    }
+
     // Check to see that the charts we need are available
     await this.$store.dispatch('catalog/load');
 
     const charts = this.$store.getters['catalog/charts'];
     const chartValues = Object.values(charts);
 
-    this.controllerChart = chartValues.find(chart => chart.chartName === 'kubewarden-controller');
+    this.controllerChart = chartValues.find(
+      chart => chart.chartName === 'kubewarden-controller'
+    );
 
     if ( this.controllerChart ) {
       this.getStartedLink = {
-        name:    'c-cluster-apps-charts-install',
+        name:   'c-cluster-apps-charts-install',
         params: {
-          cluster:  this.$route.params.cluster,
-          product:  CATALOG.APP,
+          cluster: this.$route.params.cluster,
+          product: CATALOG.APP,
         },
         query: {
           [REPO_TYPE]: this.controllerChart.repoType,
           [REPO]:      this.controllerChart.repoName,
           [CHART]:     this.controllerChart.chartName,
-          [VERSION]:   this.controllerChart.versions[0].version
-        }
+          [VERSION]:   this.controllerChart.versions[0].version,
+        },
       };
     }
   },
 
   data() {
+    const installSteps = [
+      {
+        name:  'prerequisites',
+        label: 'Prerequisites',
+        ready: false,
+      },
+      {
+        name:  'repository',
+        label: 'Repository',
+        ready: false,
+      },
+      {
+        name:  'install',
+        label: 'Install',
+        ready: false,
+      },
+    ];
+
     return {
+      errors:          [],
       controllerChart: null,
-      getStartedLink:  null
+      getStartedLink:  null,
+      initStepIndex:   0,
+      repoSchema:      null,
+      installSteps,
     };
   },
 
-  computed: { ...mapGetters['currentCluster'] }
+  computed: { ...mapGetters(['currentCluster']) },
+
+  methods: {
+    async applyCertManager(btnCb) {
+      try {
+        this.errors = [];
+
+        /*
+          fetch cert-manager latest release and apply
+        */
+        const url = '/meta/proxy/github.com/cert-manager/cert-manager/releases/download/v1.10.0/cert-manager.yaml';
+        const res = await this.$store.dispatch('management/request', { url, headers: { accept: 'application/yaml' } });
+
+        const yaml = res?.data;
+
+        await this.currentCluster.doAction('apply', { yaml, defaultNamespace: 'cert-manager' });
+
+        btnCb(true);
+
+        this.installSteps[0].ready = true;
+        this.$refs.wizard.next();
+      } catch (err) {
+        this.errors = err;
+        btnCb(false);
+      }
+    },
+
+    async addRepository(btnCb) {
+      try {
+        this.errors = [];
+
+        const repoObj = {
+          metadata: { name: 'kubewarden-chart' },
+          spec:     { url: 'https://charts.kubewarden.io', clientSecret: null },
+          type:     CATALOG.CLUSTER_REPO
+        };
+
+        await this.repoSchema.doAction('install', repoObj);
+
+        btnCb(true);
+
+        this.installSteps[1].ready = true;
+        this.$refs.wizard.next();
+      } catch (err) {
+        this.errors = err;
+        btnCb(false);
+      }
+    }
+  }
 };
 </script>
 
@@ -57,13 +161,40 @@ export default {
     <div class="container">
       <div class="title p-10">
         <div class="logo mt-20 mb-20">
-          <img src="https://www.kubewarden.io/images/logo-kubewarden.svg" height="64" />
+          <img
+            src="https://www.kubewarden.io/images/logo-kubewarden.svg"
+            height="64"
+          />
         </div>
         <div class="description">
-          {{ t('kubewarden.install.description' ) }}
+          {{ t("kubewarden.install.description") }}
         </div>
+      </div>
 
-        <template v-if="getStartedLink">
+      <InstallWizard ref="wizard" :init-step-index="initStepIndex" :steps="installSteps">
+        <template #prerequisites>
+          <h2 class="mb-10">
+            {{ t("kubewarden.install.prerequisites.certManager.title") }}
+          </h2>
+          <p class="mb-20">
+            {{ t('kubewarden.install.prerequisites.certManager.description') }}
+          </p>
+
+          <AsyncButton mode="certManager" @click="applyCertManager" />
+        </template>
+
+        <template #repository>
+          <h2 class="mb-10">
+            {{ t("kubewarden.install.prerequisites.repository.title") }}
+          </h2>
+          <p class="mb-20">
+            {{ t('kubewarden.install.prerequisites.repository.description') }}
+          </p>
+
+          <AsyncButton mode="kubewardenRepository" @click="addRepository" />
+        </template>
+
+        <template #install>
           <nuxt-link
             :to="getStartedLink"
             class="btn role-secondary mt-20"
@@ -71,24 +202,7 @@ export default {
             {{ t('kubewarden.install.getStarted') }}
           </nuxt-link>
         </template>
-
-        <template v-else>
-          <Banner color="warning" :label="t('kubewarden.install.prerequisites.repositoryWarning')" />
-        </template>
-      </div>
-
-      <hr class="m-20" />
-
-      <h2 class="mb-10">
-        {{ t('kubewarden.install.prerequisites.title') }}
-      </h2>
-      <Banner color="info" :label="t('kubewarden.install.prerequisites.certManager.description')" />
-      <div>
-        <h4 v-html="t('kubewarden.install.prerequisites.certManager.step', null, true)"></h4>
-        <CopyCode class="m-10 p-10">
-          {{ t('kubewarden.install.prerequisites.certManager.applyCommand') }}
-        </CopyCode>
-      </div>
+      </InstallWizard>
     </div>
   </div>
 </template>
