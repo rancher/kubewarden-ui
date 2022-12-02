f6c54c05c3ba62fee4b4deb4edb10161ab16f409
Jordon Leach <jordonleach@gmail.com>
2022-12-02 11:36:26 -0500

    Fix state model for policy detail views

--- pkg/kubewarden/models/policies.kubewarden.io.admissionpolicy.js
+++ pkg/kubewarden/models/policies.kubewarden.io.admissionpolicy.js
@@ -1,4 +1,36 @@
-import KubewardenModel from '../plugins/kubewarden/policy-class';
+import { colorForState, stateBackground, stateDisplay } from '@shell/plugins/dashboard-store/resource-class';
+import { get } from '@shell/utils/object';
+
+import KubewardenModel, { colorForStatus } from '../plugins/kubewarden/policy-class';
 
 export default class AdmissionPolicy extends KubewardenModel {
+  get stateDisplay() {
+    const status = get(this, 'status.policyStatus');
+
+    if ( status ) {
+      return stateDisplay(status);
+    }
+
+    return stateDisplay();
+  }
+
+  get stateBackground() {
+    const color = this.colorForState;
+
+    if ( color ) {
+      return color.replace('text-', 'bg-');
+    }
+
+    return stateBackground();
+  }
+
+  get colorForState() {
+    const status = get(this, 'status.policyStatus');
+
+    if ( status ) {
+      return colorForStatus(status);
+    }
+
+    return colorForState(this.state);
+  }
 }
