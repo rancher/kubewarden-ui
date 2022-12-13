438ffca9e81dad9f3dd1b6915011a03e3333bedf
jordojordo <jordojordo@users.noreply.github.com>
2022-12-13 21:50:29 +0000

    CI Build Artifacts

---
+++ extensions/kubewarden/0.1.12/plugin/kubewarden-0.1.12.umd.min.questions-data.js
@@ -0,0 +1,2 @@
+(("undefined"!==typeof self?self:this)["webpackJsonpkubewarden_0_1_12"]=("undefined"!==typeof self?self:this)["webpackJsonpkubewarden_0_1_12"]||[]).push([[27],{dcb4:function(e){e.exports=JSON.parse('{"categories":["storage"],"namespace":"kubewarden","questions":[]}')}}]);
+//# sourceMappingURL=kubewarden-0.1.12.umd.min.questions-data.js.map
