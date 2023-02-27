146916e83d1981937e6d547e930a68246ea9440f
Jordon Leach <jordonleach@gmail.com>
2023-02-27 13:35:24 -0500

    Add config for jest

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  setupFilesAfterEnv:   ['./jest.setup.js'],
+  modulePaths:          ['<rootDir>'],
+  moduleFileExtensions: ['js', 'json', 'vue', 'ts'],
+  moduleNameMapper:     {
+    '^~/(.*)$':         '<rootDir>/$1',
+    '^~~/(.*)$':        '<rootDir>/$1',
+    '^@/(.*)$':         '<rootDir>/$1',
+    '@shell/(.*)':      '<rootDir>/node_modules/@rancher/shell/$1',
+    '@components/(.*)':
+      '<rootDir>/node_modules/@rancher/components/dist/@rancher/components.common.js',
+    '@kubewarden/(.*)': '<rootDir>/pkg/kubewarden/$1',
+  },
+  transform: {
+    '^.+\\.js$':   '<rootDir>/node_modules/babel-jest',
+    '.*\\.(vue)$': '<rootDir>/node_modules/@vue/vue2-jest',
+    '^.+\\.tsx?$': ['ts-jest', { isolatedModules: true }],
+  },
+  transformIgnorePatterns:  ['/node_modules/(?!(@vue|@rancher|jsonpath-plus))'],
+  modulePathIgnorePatterns: [
+    '<rootDir>/scripts/',
+    '<rootDir>/assets/',
+    '<rootDir>/charts/',
+    '<rootDir>/extensions/',
+    '<rootDir>/tests/tests/',
+  ],
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+};
