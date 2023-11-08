ab20221c8e6557ae94cd33a4855a509c4446af3d
Martin Kravec <kravciak@gmail.com>
2023-11-08 10:54:02 +0100

    Add toBeAllEnabled expect

--- tests/e2e/rancher-test.ts
+++ tests/e2e/rancher-test.ts
@@ -1,4 +1,5 @@
-import { test as base } from '@playwright/test';
+import { test as baseTest, Locator } from '@playwright/test';
+import { expect as baseExpect } from '@playwright/test';
 import { RancherUI } from './components/rancher-ui';
 import { Navigation } from './components/navigation';
 import { Shell } from './components/kubectl-shell';
@@ -9,7 +10,7 @@ export type TestOptions = {
   shell: Shell
 };
 
-export const test = base.extend<TestOptions>({
+export const test = baseTest.extend<TestOptions>({
   ui: async ({ page }, use) => {
     use(new RancherUI(page));
   },
@@ -21,4 +22,45 @@ export const test = base.extend<TestOptions>({
   },
 });
 
-export { expect } from '@playwright/test';
+export const expect = baseExpect.extend({
+
+  async toBeAllEnabled(locator: Locator, options?: { enabled?: boolean }) {
+    const assertionName = 'toBeAllEnabled';
+    let pass: boolean;
+    const expected = options?.enabled == true ? 'enabled' : 'disabled';
+    let matcherResult: any;
+    try {
+      await locator.locator('input,button').first().waitFor({state: 'attached'})
+      await test.step(assertionName, async () => {
+        for (const e of await locator.locator('input,button').all()) {
+          await baseExpect(e).toBeEnabled(options)
+        }
+      })
+      pass = true;
+    } catch (e: any) {
+      matcherResult = e.matcherResult;
+      pass = false;
+    }
+
+    const message = pass
+      ? () => this.utils.matcherHint(assertionName, undefined, undefined) +
+        '\n\n' +
+        `Locator: ${locator}\n` +
+        `Expected: ${this.utils.printExpected(expected)}\n` +
+        (matcherResult ? `Received: ${this.utils.printReceived(matcherResult.actual)}` : '')
+      : () => this.utils.matcherHint(assertionName, undefined, undefined) +
+        '\n\n' +
+        `Locator: ${locator}\n` +
+        `Expected: ${this.utils.printExpected(expected)}\n` +
+        (matcherResult ? `Received: ${this.utils.printReceived(matcherResult.actual)}` : '');
+
+    return {
+      message,
+      pass,
+      name: assertionName,
+      expected,
+      actual: matcherResult?.actual,
+    };
+  },
+
+});
