1073445aae629eb66b2825f78f6d927330b2cd49
Jordon Leach <jordonleach@gmail.com>
2025-01-28 11:11:44 -0500

    Replace ArtifactHub package fetching with helm charts

--- pkg/kubewarden/types/kubewarden.ts
+++ pkg/kubewarden/types/kubewarden.ts
@@ -10,7 +10,8 @@ export const CHART_NAME = 'rancher-kubewarden';
 
 export const KUBEWARDEN_DASHBOARD = 'dashboard';
 export const KUBEWARDEN_REPO = 'https://charts.kubewarden.io';
-export const REGO_POLICIES_REPO = 'https://github.com/kubewarden/rego-policies-library';
+export const KUBEWARDEN_CHARTS_REPO = 'https://github.com/kubewarden/helm-charts';
+export const KUBEWARDEN_CHARTS_REPO_GIT = 'https://github.com/kubewarden/helm-charts.git';
 
 export const KUBEWARDEN_CHARTS = {
   CONTROLLER:       'kubewarden-controller',
