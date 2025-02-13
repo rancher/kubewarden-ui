5fc104ba5e29d6100bedd1ad48ff511a6ece503d
Jordon Leach <jordonleach@gmail.com>
2025-02-13 10:12:30 -0500

    Add 302 redirect to successful whitelist domain fetch

--- pkg/kubewarden/utils/determine-airgap.ts
+++ pkg/kubewarden/utils/determine-airgap.ts
@@ -29,33 +29,28 @@ export async function isAirgap(context: AirgapConfig): Promise<Boolean> {
         redirectUnauthorized: false,
       });
 
-      if ( res._status !== 200 ) {
-        store.dispatch('kubewarden/updateAirGapped', true);
+      const status = res?._status
 
-        return true;
+      if (status === 200 || status === 302) {
+        store.dispatch('kubewarden/updateAirGapped', false);
+
+        return false;
       }
 
-      store.dispatch('kubewarden/updateAirGapped', false);
+      store.dispatch('kubewarden/updateAirGapped', true);
 
-      return false;
+      return true;
     }
   } catch (e) {
     if ( !store.getters['kubewarden/airGapped'] ) {
-      const error = {
-        _statusText: 'Warning',
-        message:     'Unable to determine management.cattle.io.settings/whitelist-domain value. Some Kubewarden UI features may be unavailable.'
-      };
-
-      handleGrowl({
-        error, store, type: 'warning'
-      });
-      store.dispatch('kubewarden/updateAirGapped', true);
+      console.log('Unable to determine management.cattle.io.settings/whitelist-domain value.', e); // eslint-disable-line no-console
+      store.dispatch('kubewarden/updateAirGapped', false);
     }
 
-    return true;
+    return false;
   }
 
-  store.dispatch('kubewarden/updateAirGapped', true);
+  store.dispatch('kubewarden/updateAirGapped', false);
 
-  return true;
+  return false;
 }
