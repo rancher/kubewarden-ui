cfa6a4e76a1d351c7bac997a97744b1dff6f4946
Martin Kravec <kravciak@gmail.com>
2025-08-14 09:23:44 +0200

    Tests for Labels & Annotations policy
    Signed-off-by: Martin Kravec <kravciak@gmail.com>

--- tests/e2e/pages/policies.page.ts
+++ tests/e2e/pages/policies.page.ts
@@ -8,7 +8,8 @@ export const apList = ['Custom Policy', 'Allow Privilege Escalation PSP', 'Allow
   'Deprecated API Versions', 'Disallow Service Loadbalancer', 'Disallow Service Nodeport', 'Echo', 'Environment Variable Secrets Scanner', 'Environment Variable Policy', 'Flexvolume Drivers Psp',
   'Host Namespaces PSP', 'Hostpaths PSP', 'Ingress Policy', 'Namespace label propagator', 'Pod Privileged Policy', 'Pod Runtime', 'Readonly Root Filesystem PSP', 'Share PID namespace',
   'Safe Annotations', 'Safe Labels', 'Seccomp PSP', 'Selinux PSP', 'Sysctl PSP', 'Trusted Repos', 'User Group PSP', 'Verify Image Signatures', 'volumeMounts', 'Volumes PSP', 'Unique Ingress host',
-  'Unique service selector', 'PVC StorageClass Validator', 'CEL Policy', 'Pod ndots', 'Do not expose admission controller webhook services', 'Priority class policy', 'High Risk Service Account'] as const
+  'Unique service selector', 'PVC StorageClass Validator', 'CEL Policy', 'Pod ndots', 'Do not expose admission controller webhook services', 'Priority class policy', 'High Risk Service Account',
+  'Labels', 'Annotations'] as const
 
 export const capList = [...apList, 'PSA Label Enforcer'] as const
 
