437d95d9e7518c3b2dbbbafbcfc5329d01c713c2
Steven Zhang <steven.zhang@suse.com>
2025-10-22 19:37:23 -0700

    Make package name consist with release build tag name

--- pkg/sbomscanner/types/registry.ts
+++
@@ -1,7 +0,0 @@
-export const REGISTRY_STATUS = {
-  PENDING:     'pending',
-  SCHEDULED:   'scheduled',
-  IN_PROGRESS: 'inprogress',
-  COMPLETE:    'complete',
-  FAILED:      'failed',
-};
--- pkg/sbomscanner/types/vex.ts
+++
@@ -1,4 +0,0 @@
-export const VEX_STATUS = {
-  ENABLED:  'enabled',
-  DISABLED: 'disabled',
-};
