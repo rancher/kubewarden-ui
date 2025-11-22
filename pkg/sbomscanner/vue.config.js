fa1deeff03660aa2a4395563425f9ade4b90bc52
Steven Zhang <steven.zhang@suse.com>
2025-11-21 17:34:57 -0800

    fix vue build issue on release

--- pkg/sbomscanner/vue.config.js
+++ pkg/sbomscanner/vue.config.js
@@ -1,15 +1,37 @@
+const path = require('path');
 const webpack = require('webpack');
-const baseConfig = require('./.shell/pkg/vue.config')(__dirname);
+const vendorConfigFactory = require('./.shell/pkg/vue.config');
 
-module.exports = {
-  ...baseConfig,
-  chainWebpack: (config) => {
-    if (typeof baseConfig.chainWebpack === 'function') {
-      baseConfig.chainWebpack(config);
+module.exports = () => {
+  const vendorConfig =
+    typeof vendorConfigFactory === 'function' ? vendorConfigFactory(__dirname) : vendorConfigFactory;
+
+  // Wrap the vendor configureWebpack if it exists.
+  const vendorConfigureWebpack = vendorConfig.configureWebpack;
+  const customConfigureWebpack = (config) => {
+    if (typeof vendorConfigureWebpack === 'function') {
+      vendorConfigureWebpack(config);
     }
+    // Add the @sbomscanner alias to point to the pkg/sbomscanner directory.
+    config.resolve.alias['@sbomscanner'] = path.resolve(__dirname);
+  };
 
+  // Create an override for __tests__ directories.
+  const kwChainWebpack = (config) => {
     config.plugin('ignore-tests')
       .use(webpack.IgnorePlugin, [{ resourceRegExp: /[\\/]__tests__[\\/]/ }]);
-  },
-  configureWebpack: baseConfig.configureWebpack,
+  };
+
+  const mergedChainWebpack = (config) => {
+    if (typeof vendorConfig.chainWebpack === 'function') {
+      vendorConfig.chainWebpack(config);
+    }
+    kwChainWebpack(config);
+  };
+
+  // Merge our custom chainWebpack and configureWebpack with the vendor config.
+  return Object.assign({}, vendorConfig, {
+    chainWebpack:     mergedChainWebpack,
+    configureWebpack: customConfigureWebpack,
+  });
 };
