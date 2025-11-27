179f4b68432a2c47c84ecd244f08c4db1e4e6dfd
cnotv <giuseppe.leo@suse.com>
2025-11-27 16:52:16 +0100

    Abstract components and i18n

---
+++ pkg/kubewarden/components/Dashboard/EmptyPolicies.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RcButton } from '@rancher/components';
+import { useStore } from 'vuex';
+import { useI18n } from '@shell/composables/useI18n';
+
+const store = useStore();
+const { t } = useI18n(store);
+
+defineProps<{
+  label: string;
+}>();
+</script>
+
+<template>
+  <div>
+    <p>{{ label }}</p>
+    <RcButton link>{{ t('kubewarden.dashboard.cards.generic.new') }}</RcButton>
+  </div>
+</template>
---
+++ pkg/kubewarden/components/Dashboard/PoliciesSummary.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useStore } from 'vuex';
+import { useI18n } from '@shell/composables/useI18n';
+
+const store = useStore();
+const { t } = useI18n(store);
+
+defineProps<{
+  protect: number;
+  monitor: number;
+}>();
+</script>
+
+<template>
+  <p>{{ t('kubewarden.generic.policies') }} {{ protect }} {{ t('kubewarden.generic.protect') }} + {{ monitor }} {{ t('kubewarden.generic.monitor') }}</p>
+</template>
