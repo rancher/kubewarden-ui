5279f5be306365a17feb55310fce0532973b42c6
cnotv <giuseppe.leo@suse.com>
2025-11-27 18:01:52 +0100

    Update PoliciesSummary to match design

--- pkg/kubewarden/components/Dashboard/PoliciesSummary.vue
+++ pkg/kubewarden/components/Dashboard/PoliciesSummary.vue
@@ -8,9 +8,32 @@ const { t } = useI18n(store);
 defineProps<{
   protect: number;
   monitor: number;
+  link: string;
 }>();
 </script>
 
 <template>
-  <p>{{ t('kubewarden.generic.policies') }} {{ protect }} {{ t('kubewarden.generic.protect') }} + {{ monitor }} {{ t('kubewarden.generic.monitor') }}</p>
+  <div class="policies-summary">
+    <span>{{ t('kubewarden.generic.policies') }}</span>
+    <div class="policies-summary__stats">
+      <span>{{ protect }} {{ t('kubewarden.generic.protect') }}</span>
+      <span>+</span>
+      <router-link
+        :to="link"
+      >{{ monitor }} {{ t('kubewarden.generic.monitor') }}</router-link>
+    </div>
+  </div>
 </template>
+<style scoped lang="scss">
+.policies-summary {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+
+  &__stats {
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+  }
+}
+</style>
