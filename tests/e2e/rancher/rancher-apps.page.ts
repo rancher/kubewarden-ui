9744490483d47f9064361c5b8906fb17e677af48
Martin Kravec <kravciak@gmail.com>
2026-02-18 10:39:57 +0100

    Rancher 2.14 test fixes

--- tests/e2e/rancher/rancher-apps.page.ts
+++ tests/e2e/rancher/rancher-apps.page.ts
@@ -186,6 +186,10 @@ export class RancherAppsPage extends BasePage {
     if (options?.navigate !== false) {
       await this.nav.explorer('Apps', 'Charts')
       await expect(this.page.getByRole('heading', { name: 'Charts', exact: true })).toBeVisible()
+      // Handle infinite list scrolling
+      if (!await card.isVisible()) {
+        await this.page.getByTestId('charts-filter-input').fill(chart.title)
+      }
       await card.click()
 
       if (chart.version) {
@@ -237,7 +241,8 @@ export class RancherAppsPage extends BasePage {
       await this.nav.explorer('Apps', 'Installed Apps')
       await expect(this.page.getByRole('heading', { name: 'Installed Apps' })).toBeVisible()
 
-      await this.ui.tableRow(name).action('Edit/Upgrade')
+      // Edit/Upgrade -> Edit/Change version (Rancher 2.14+)
+      await this.ui.tableRow(name).action(/^\s*Edit/)
       await expect(this.page.getByRole('heading', { name })).toBeVisible()
     }
 
